e08b9bafbb35416bd2f46bb3fa55aaf26c2f62e6
mattowsh <5089@holbertonschool.com>
2022-08-22 11:39:36 -0700

    print_array in the correct line final version

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,8 +20,8 @@ void bubble_sort(int *array, size_t size)
 				aux = array[i + 1];
 				array[i + 1] = array[i];
 				array[i] = aux;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 	}
 }
