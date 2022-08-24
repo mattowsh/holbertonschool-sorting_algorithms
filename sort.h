2f5ba4aeae2ed060f1c0ae58899ae5cae55b9189
mattowsh <5089@holbertonschool.com>
2022-08-24 10:51:00 -0700

    task 3 advanced

--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swapnodes(listint_t *nodeA, listint_t *nodeB, listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
